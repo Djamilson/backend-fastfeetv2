2ec9aee8a80ac066e7be0eb7b72d81454da039f8
Djamilson <djamilson@gmail.com>
2020-04-25 01:55:18 -0300

    Redefinindo Pagination

--- src/app/models/Order.js
+++ src/app/models/Order.js
@@ -9,6 +9,8 @@ import {
   isAfter,
 } from 'date-fns';
 
+import CompareHour from '../util/compareHour';
+
 class Order extends Model {
   static init(sequelize) {
     super.init(
@@ -79,54 +81,7 @@ class Order extends Model {
         withdrawProduct: {
           type: Sequelize.VIRTUAL,
           get() {
-            const newDate = new Date();
-
-            const date = format(newDate, "yyyy-MM-dd'T'HH:mm:ssxxx");
-            const parsedDate = parseISO(date);
-            const dateToCompare = addHours(parsedDate, 3);
-
-            //hora de inicio 8horas //add 3 hours
-            const compDateStart = addHours(
-              new Date(
-                getYear(newDate),
-                getMonth(newDate),
-                getDate(newDate),
-                8,
-                0,
-                0
-              ),
-              3
-            );
-
-            //hora do fim as 18horas
-            const compDateEnd = addHours(
-              new Date(
-                getYear(newDate),
-                getMonth(newDate),
-                getDate(newDate),
-                21,
-                0,
-                0
-              ),
-              3
-            );
-
-            const horaStart = isAfter(
-              new Date(dateToCompare),
-              new Date(compDateStart)
-            );
-            const horaEnd = isAfter(
-              new Date(compDateEnd),
-              new Date(dateToCompare)
-            );
-
-            let withdrawProduct = false;
-
-            if (horaStart === true && horaEnd === true) {
-              withdrawProduct = true;
-            }
-
-            return withdrawProduct;
+            return CompareHour.run();
           },
         },
       },
